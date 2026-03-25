c884093c7ee347c944e2275da7b0024e4dc475df
songs <hhsong@thinkm.co.kr>
2026-03-25 20:09:42 +0900

    16KB 오류 수정

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -4,6 +4,8 @@ APP_ABI := armeabi-v7a arm64-v8a
 #APP_ABI := armeabi-v7a
 APP_PLATFORM := android-14
 #APP_STL := stlport_static
-APP_STL := c++_shared
+APP_STL := c++_static
 APP_OPTIM := debug
 APP_CFLAGS += -Wno-error=format-security
+APP_LDFLAGS += -Wl,-z,max-page-size=16384
+APP_LDFLAGS += -Wl,-z,common-page-size=16384
